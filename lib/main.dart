8d319278be2023c1c95e4e7c3577c0fe2d7a1d89
heir <71329348+he1r@users.noreply.github.com>
2022-01-08 01:23:00 +0100

    added text fields and buttons

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(
           title: "DataBaza",
-      )
+      ),
     );
   }
 }
@@ -35,50 +35,215 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
-        title: Text(widget.title),
         centerTitle: true,
+        title: const Text("DataBaza"),
         backgroundColor: Colors.green,
         foregroundColor: Colors.black,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(25),
-        child: Container(
-          padding: EdgeInsets.only(left: 50, right: 60),
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.black,
-              width: 3,
-            ),
-              borderRadius: BorderRadius.circular(15)
-          ),
-          child: DropdownButton<String>(
-            value: valuex,
-            icon: Icon(Icons.arrow_drop_down),
-            iconEnabledColor: Colors.black,
-            iconSize: 40,
-            isExpanded: true,
-            underline: SizedBox(),
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 15,
-            ),
-            dropdownColor: Colors.grey,
-            onChanged: (value) {
-            setState((){
-            valuex = value!;
-               });
-            },
-            hint: const Text("Zgjidh DataBazen"),
-            items: listItem.map((valueItem) {
-              return DropdownMenuItem<String>(
-                value: valueItem,
-                child: Text(valueItem),
-              );
-            }).toList(),
-          ),
-        ),
-      ),
+
+        body: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: <Widget>[
+
+              Padding(
+                padding: const EdgeInsets.all(20),
+
+                child: DropdownButton<String>(
+                  value: valuex,
+
+                  icon: const Icon(Icons.arrow_drop_down),
+                  iconEnabledColor: Colors.black,
+                  iconSize: 40,
+                  isExpanded: true,
+
+                  underline: const SizedBox(),
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                  ),
+
+                  items: listItem.map((valueItem) {
+                    return DropdownMenuItem<String>(
+                      value: valueItem,
+                      child: Text(valueItem),
+                    );
+                  }).toList(),
+
+                  onChanged: (value) {
+                    setState((){
+                      valuex = value!;
+                    });
+                  },
+                )
+              ),
+
+              const Padding(
+                padding: EdgeInsets.all(1),
+
+                child: TextField(
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    errorBorder: InputBorder.none,
+                    disabledBorder: InputBorder.none,
+                    labelText: 'Emri',
+                      ),
+                  ),
+              ),
+
+                const Padding(
+                 padding: EdgeInsets.all(1),
+
+                 child: TextField(
+                   cursorColor: Colors.black,
+                   decoration: InputDecoration(
+                   border: InputBorder.none,
+                   focusedBorder: InputBorder.none,
+                   enabledBorder: InputBorder.none,
+                   errorBorder: InputBorder.none,
+                   disabledBorder: InputBorder.none,
+                   labelText: 'Mbiemri',
+                    ),
+                  ),
+               ),
+
+              const Padding(
+                padding: EdgeInsets.all(1),
+
+                child: TextField(
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                  labelText: 'Atesia',
+                ),
+                ),
+              ),
+
+              const Padding(
+                padding: EdgeInsets.all(1),
+
+                child: TextField(
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                  labelText: 'Amesia',
+                ),
+                ),
+              ),
+
+              const Padding(
+                padding: EdgeInsets.all(1),
+
+                child: TextField(
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                  labelText: 'Datelindja',
+                ),
+                ),
+              ),
+
+              const Padding(
+                padding: EdgeInsets.all(1),
+
+                child: TextField(
+                  cursorColor: Colors.black,
+
+                  decoration: InputDecoration(
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                  labelText: 'Id Kryefamiljari',
+                ),
+                ),
+              ),
+
+              Row(
+                children:  [
+
+                  Expanded(
+                    child: Container(
+                      height: 50.0,
+                      margin: const EdgeInsets.all(10),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Ink(
+                          decoration: BoxDecoration(
+                              gradient: const LinearGradient(
+                                colors: [Color(0xff374ABE), Color(0xff64B6FF)],
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                              ),
+                              borderRadius: BorderRadius.circular(30.0)),
+                          child: Container(
+                            constraints:
+                            const BoxConstraints(maxWidth: 250.0, minHeight: 50.0),
+                            alignment: Alignment.center,
+                            child: const Text(
+                              "Clear",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white, fontSize: 15),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Expanded(
+                    child: Container(
+                      height: 50.0,
+
+                      margin: const EdgeInsets.all(50),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Ink(
+                          decoration: BoxDecoration(
+                              gradient: const LinearGradient(
+                                colors: [Color(0xff374ABE), Color(0xff64B6FF)],
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                              ),
+                              borderRadius: BorderRadius.circular(30.0)),
+                          child: Container(
+                            constraints:
+                            const BoxConstraints(maxWidth: 250.0, minHeight: 50.0),
+                            alignment: Alignment.center,
+                            child: const Text(
+                              "Search",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white, fontSize: 15),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+            ],
+          )
+        )
     );
   }
 }
