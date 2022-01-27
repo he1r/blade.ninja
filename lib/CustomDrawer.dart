3eccf2b38fe0cbb13886102a82a5adfda89c3cb0
heir <71329348+he1r@users.noreply.github.com>
2022-01-27 17:25:07 +0100

    fixed side menu

--- lib/CustomDrawer.dart
+++ lib/CustomDrawer.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'UIMain.dart';
 
@@ -7,56 +9,123 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      
+      backgroundColor: Colors.indigo[900],
       child: Column(
         children: <Widget>[
+          const SizedBox(
+            height: 20,
+          ),
           Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(20),
-            color: Theme.of(context).primaryColor,
+              width: double.infinity,
+              color: Colors.indigo[900],
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                "DataBaza",
+                style: GoogleFonts.bebasNeue(
+                  color: Colors.white,
+                  letterSpacing: 6,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+                textAlign: TextAlign.center,
+              )),
+          const SizedBox(
+            height: 15,
           ),
           ListTile(
-            leading: const Icon(Icons.person),
-            title: const Text("Gjendja Civile 2008"),
+            leading: const Icon(
+              Icons.person,
+              color: Colors.white,
+            ),
+            title: Text("Gjendja Civile 2008",
+                style:
+                    GoogleFonts.josefinSans(color: Colors.white, fontSize: 15)),
             onTap: () {
-              Navigator.push(context, MaterialPageRoute(
-                builder: (context) => UIMain(0),),
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => UIMain(0),
+                ),
               );
             },
           ),
+          const SizedBox(
+            height: 15,
+          ),
           ListTile(
-            leading: const Icon(Icons.person_add_alt),
-            title: const Text("Patronazhisti 2021"),
+            leading: const Icon(
+              Icons.person_add_alt,
+              color: Colors.white,
+            ),
+            title: Text("Patronazhisti 2021",
+                style:
+                    GoogleFonts.josefinSans(color: Colors.white, fontSize: 15)),
             onTap: () {
-              Navigator.push(context, MaterialPageRoute(
-                builder: (context) => UIMain(1),),
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => UIMain(1),
+                ),
               );
             },
           ),
+          const SizedBox(
+            height: 15,
+          ),
           ListTile(
-            leading: const Icon(Icons.car_repair_rounded),
-            title: const Text("Targat 2021"),
+            leading: const Icon(
+              Icons.car_repair_rounded,
+              color: Colors.white,
+            ),
+            title: Text("Targat 2021",
+                style:
+                    GoogleFonts.josefinSans(color: Colors.white, fontSize: 15)),
             onTap: () {
-              Navigator.push(context, MaterialPageRoute(
-                builder: (context) => UIMain(2),),
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => UIMain(2),
+                ),
               );
             },
           ),
+          const SizedBox(
+            height: 15,
+          ),
           ListTile(
-            leading: const Icon(Icons.person_search_sharp),
-            title: const Text("Targat + Patronazhisti 2021"),
+            leading: const Icon(
+              Icons.person_search_sharp,
+              color: Colors.white,
+            ),
+            title: Text("Targat + Patronazhisti",
+                style:
+                    GoogleFonts.josefinSans(color: Colors.white, fontSize: 15)),
             onTap: () {
-              Navigator.push(context, MaterialPageRoute(
-                builder: (context) => UIMain(3),),
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => UIMain(3),
+                ),
               );
             },
           ),
+          const SizedBox(
+            height: 15,
+          ),
           ListTile(
-            leading: const Icon(Icons.money),
-            title: const Text("Rrogat Prill"),
+            leading: const Icon(
+              Icons.money,
+              color: Colors.white,
+            ),
+            title: Text("Rrogat Prill",
+                style:
+                    GoogleFonts.josefinSans(color: Colors.white, fontSize: 15)),
             onTap: () {
-              Navigator.push(context, MaterialPageRoute(
-                builder: (context) => UIMain(4),),
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => UIMain(4),
+                ),
               );
             },
           ),
--- lib/UIMain.dart
+++ lib/UIMain.dart
@@ -1,31 +1,36 @@
+// ignore_for_file: avoid_print
+
 import 'dart:convert';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'CustomDrawer.dart';
 import 'main.dart';
 
+// ignore: must_be_immutable
 class UIMain extends StatelessWidget {
-
+  // ignore: prefer_typing_uninitialized_variables
   var fields;
-  var controllers=[];
-  var dbIndex=0;
-  var checkboxVal=0; // temporary
+  var controllers = [];
+  var dbIndex = 0;
+  var checkboxVal = 0; // temporary
 
   UIMain(this.dbIndex, {Key? key}) : super(key: key) {
-      fields = databazat[dbIndex][1];
+    fields = databazat[dbIndex][1];
     for (int i = 0; i < fields.length; i++) {
       controllers.add(TextEditingController(text: ""));
     }
   }
-  
+
   Map generateJson() {
     final Map<String, Object> jsonData;
     jsonData = {
       "db": dbIndex,
     };
     for (int i = 0; i < fields.length; i++) {
-      jsonData[fields[i][2].toString()]=[fields[i][1]+checkboxVal, controllers[i].text];
+      jsonData[fields[i][2].toString()] = [
+        fields[i][1] + checkboxVal,
+        controllers[i].text
+      ];
     }
     print(jsonData);
     return jsonData;
@@ -43,24 +48,22 @@ class UIMain extends StatelessWidget {
     });
   }
 
-  void setDateText(var controller, var date){
-    controller.text=date;
+  void setDateText(var controller, var date) {
+    controller.text = date;
   }
 
   Future<void> _selectDate(BuildContext context, var controller) async {
-    var selectedDate=DateTime.now().toString();
+    var selectedDate = DateTime.now().toString();
     if (controller.text.isNotEmpty) {
       selectedDate = controller.text;
-      print("----->>>>>"+selectedDate);
+      print("----->>>>>" + selectedDate);
     }
     var startDate = DateTime.now();
     try {
       startDate = DateTime.parse(controller.text);
     }
     // ignore: empty_catches
-    on Exception{
-
-    }
+    on Exception {}
 
     final DateTime? picked = await showDatePicker(
       context: context,
@@ -69,62 +72,72 @@ class UIMain extends StatelessWidget {
       lastDate: DateTime.now(),
     );
 
-    if (picked != null){
+    if (picked != null) {
       setDateText(controller, picked.toString().split(" ")[0]);
-      print("---->>"+picked.toString());
+      print("---->>" + picked.toString());
       selectedDate = picked.toString();
     }
   }
 
-  List<Widget> generateInputFields(BuildContext context){
+  List<Widget> generateInputFields(BuildContext context) {
     List<Widget> data = [];
-    for( int i = 0 ; i < fields.length ; i++) {
+    for (int i = 0; i < fields.length; i++) {
       if (fields[i][2] == 'Datelindja') {
         data.add(
-            Stack(
+          Stack(
+            children: [
+              Row(
                 children: [
-                  Row(
-                    children: [
-                      const SizedBox(width: 4, height: 1,),
-                      // simple space
-                      Expanded(
-                        //                        padding: const EdgeInsets.all(1),
-                        child: TextField(
+                  const SizedBox(
+                    width: 4,
+                    height: 1,
+                  ),
+                  // simple space
+                  Expanded(
+                    //                        padding: const EdgeInsets.all(1),
+                    child: TextField(
 //                          enabled: false,
-                          decoration: InputDecoration(
+                      decoration: InputDecoration(
 //                            floatingLabelBehavior: FloatingLabelBehavior.always,
 //                            hintText: "dd/mm/yyyy",
-                            labelText: fields[i][0].toString(),
-                          ),
-                          controller: controllers[i],
-                        ),
+                        labelText: fields[i][0].toString(),
                       ),
-                      const SizedBox(width: 60, height: 2,),
-
-                      // space where the button is meant to go
-                    ],
+                      controller: controllers[i],
+                    ),
                   ),
-                  Row(
-                  children: [
-                    Expanded(
-                      // ignore: deprecated_member_use
-                      child: FlatButton(
-                        onPressed: () => _selectDate(context, controllers[i]),
-                        child: Text(""),
+                  const SizedBox(
+                    width: 60,
+                    height: 2,
+                  ),
 
-                      ),
-                      ),
-                    const SizedBox(width: 60, height: 2,),
-                    ],
-                ),
-              ],
-            ),
+                  // space where the button is meant to go
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    // ignore: deprecated_member_use
+                    child: FlatButton(
+                      onPressed: () => _selectDate(context, controllers[i]),
+                      child: const Text(""),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 60,
+                    height: 2,
+                  ),
+                ],
+              ),
+            ],
+          ),
         );
-      }
-      else {
+      } else {
         data.add(Row(
           children: [
-            const SizedBox(width: 4, height: 1,),
+            const SizedBox(
+              width: 4,
+              height: 1,
+            ),
             // simple space
             Expanded(
               //                        padding: const EdgeInsets.all(1),
@@ -135,12 +148,14 @@ class UIMain extends StatelessWidget {
                 controller: controllers[i],
               ),
             ),
-            const SizedBox(width: 60, height: 2,),
+            const SizedBox(
+              width: 60,
+              height: 2,
+            ),
             // space where the button is meant to go
           ],
-        )
-        );
-      };
+        ));
+      }
     }
     return data;
   }
@@ -151,15 +166,15 @@ class UIMain extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
-            backgroundColor: Color(0xff1f252e),
+          backgroundColor: Color(0xff1f252e),
         ),
-        scaffoldBackgroundColor: Color(0xff1f2836),
+        scaffoldBackgroundColor: const Color(0xff1f2836),
         inputDecorationTheme: const InputDecorationTheme(
           contentPadding: EdgeInsets.all(10),
           border:
-            UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+              UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
           disabledBorder:
-            UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+              UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
           enabledBorder:
               UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
           focusedBorder:
@@ -174,12 +189,11 @@ class UIMain extends StatelessWidget {
             fontSize: 18,
             decorationColor: Colors.red,
           ),
-
         ),
         textSelectionTheme: const TextSelectionThemeData(
           cursorColor: Colors.white,
-            selectionColor: Colors.white,
-            selectionHandleColor: Colors.white,
+          selectionColor: Colors.white,
+          selectionHandleColor: Colors.white,
         ),
         textTheme: const TextTheme(
 //          bodyText1: TextStyle(color: Colors.white),
@@ -195,8 +209,6 @@ class UIMain extends StatelessWidget {
 //          headline6: TextStyle(color: Colors.white),
         ),
       ),
-
-
       home: Scaffold(
           resizeToAvoidBottomInset: false,
           appBar: AppBar(
@@ -215,7 +227,7 @@ class UIMain extends StatelessWidget {
                   Row(
                     children: [
                       Container(
-                        margin: EdgeInsets.all(10),
+                        margin: const EdgeInsets.all(10),
                         height: 40.0,
                         width: 40.0,
                         child: ElevatedButton.icon(
@@ -224,20 +236,25 @@ class UIMain extends StatelessWidget {
                             color: Colors.white,
                             size: 25.0,
                           ),
-                          label: Text(''),
+                          label: const Text(''),
                           style: ElevatedButton.styleFrom(
                             alignment: Alignment.center,
-                            padding:  EdgeInsets.only(left: 6),
-                            primary:  const Color(0xff0b51d09),
+                            padding: const EdgeInsets.only(left: 6),
+                            primary: const Color(0xff0b51d09),
                             shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(50.0),
-                                side: const BorderSide(color: Color(0xff0b51d09))),
+                                side: const BorderSide(
+                                    color: Color(0xff0b51d09))),
                           ),
-                          onPressed: (){for(int i=0; i<fields.length; i++){controllers[i].text="";}},
+                          onPressed: () {
+                            for (int i = 0; i < fields.length; i++) {
+                              controllers[i].text = "";
+                            }
+                          },
                         ),
                       ),
                       Container(
-                        margin: EdgeInsets.all(10),
+                        margin: const EdgeInsets.all(10),
                         height: 40.0,
                         width: 40.0,
                         child: ElevatedButton.icon(
@@ -246,14 +263,15 @@ class UIMain extends StatelessWidget {
                             color: Colors.white,
                             size: 25.0,
                           ),
-                          label: Text(''),
+                          label: const Text(''),
                           style: ElevatedButton.styleFrom(
                             alignment: Alignment.center,
-                            padding:  EdgeInsets.only(left: 6),
-                            primary:  const Color(0xff2769c4),
+                            padding: const EdgeInsets.only(left: 6),
+                            primary: const Color(0xff2769c4),
                             shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(50.0),
-                                side: const BorderSide(color: Color(0xff2769c4))),
+                                side:
+                                    const BorderSide(color: Color(0xff2769c4))),
                           ),
                           onPressed: data,
                         ),
